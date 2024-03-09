a09834f4a7252c152538509eb845880a86196f7a
Michael Mortlock-Chapman (z5359078) <z5359078@unsw.edu.au>
2024-03-09 14:31:48 +1100

    fixed clinet3.py and added unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,14 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    actual = getDataPoint(quotes[0])
+    expected = (
+      quotes[0]["stock"],
+      quotes[0]["top_bid"]["price"],
+      quotes[0]["top_ask"]["price"],
+      (quotes[0]["top_bid"]["price"] + quotes[0]["top_ask"]["price"]) / 2
+    )
+    self.assertEqual(actual, expected)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +23,23 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    stock, bid_price, ask_price, price = getDataPoint(quotes[0])
+    self.assertEqual(price, (quotes[0]["top_bid"]["price"] + quotes[0]["top_ask"]["price"]) / 2)
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateRatio(self):
+    priceABC = 1; priceDEF = 2
+    ratio = getRatio(priceABC, priceDEF)
+    self.assertEqual(ratio, priceABC / priceDEF)
+
+  def test_getRatio_zeroDivisor(self):
+    priceABC = 1; priceDEF = 0
+    self.assertIsNone(getRatio(priceABC, priceDEF))
+
+  def test_getRatio_zeroDividend(self):
+    priceABC = 0; priceDEF = 1
+    self.assertIsNotNone(getRatio(priceABC, priceDEF))
 
 
 
